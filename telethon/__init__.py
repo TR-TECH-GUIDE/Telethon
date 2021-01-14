c38db1103db95be07580a61de71695f0b226b0f4
Lonami Exo <totufals@hotmail.com>
2021-01-14 22:26:31 +0100

    Attempt to solve #1669

--- telethon/__init__.py
+++ telethon/__init__.py
@@ -1,8 +1,10 @@
 from .client.telegramclient import TelegramClient
 from .network import connection
-from .tl import types, functions, custom
-from .tl.custom import Button
+from .tl import types, functions
 from . import version, events, utils, errors
+# It is important that this is after the utils import to avoid circular imports (#1669).
+from .tl import custom
+from .tl.custom import Button
 
 __version__ = version.__version__
 
